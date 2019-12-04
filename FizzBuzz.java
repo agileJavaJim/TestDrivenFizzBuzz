76532912cf5c82911f46470261f15ce614e580dc
Jim Robinson <55302546+agileJavaJim@users.noreply.github.com>
2019-12-03 19:30:14 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 	public String myFizzBuzz(int i) {
 		if(i % 3 == 0 && i % 5 == 0) {
 			System.out.println("Fizz Buzz");
-			return "Fizz Buzz";
+		return "Fizz Buzz";
 		}
 		else if(i % 3 == 0) {
 			System.out.println("Fizz");
@@ -14,6 +14,15 @@ public class FizzBuzz {
 			System.out.println("Buzz");
 		return "Buzz";	
 		}
+		else{
+			System.out.println("Buzz");
+			myFizzBuzzNum(i);	
+		}
 		return null;
+	}
+	
+	public int myFizzBuzzNum(int i) {
+		System.out.println(i);
+	return i;
 	}
 }
